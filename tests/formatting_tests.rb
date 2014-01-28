7a74209efd7fbd7de6dbee1424d25386e309b6f7
Baptiste Fontaine <batifon@yahoo.fr>
2014-01-28 15:45:58 +0100

    formatting tests

---
+++ tests/formatting_tests.rb
@@ -0,0 +1,60 @@
+# -*- coding: UTF-8 -*-
+
+class Meuh_formatting_test < Test::Unit::TestCase
+
+  def setup
+    @tracks = {
+      :previous => [
+        {
+          :artist => 'Artist1',
+          :album =>  'Album1',
+          :title =>  'Track1',
+          :time =>   '00:01:01',
+        },
+        {
+          :artist => 'Artist2',
+          :album =>  'Album2',
+          :title =>  'Track2',
+          :time =>   '00:01:00',
+        },
+        {
+          :artist => 'Artist3',
+          :album =>  'Album3',
+          :title =>  'Track3',
+          :time =>   '00:00:45',
+        }
+      ],
+      :current => {
+        :artist => nil,
+        :album => 'CurrBar',
+        :title => 'Current Track',
+        :time => '00:01:02'
+      },
+      :next => [
+        {
+          :artist => 'Foo',
+          :album  => 'FooBar',
+          :title  => 'Bar',
+          :time   => ''
+        }
+      ]
+    }
+  end
+
+  def test_general_format_no_color
+    r = /^.+\n\nPreviously:\n(?:\* \[.*?\] .*\n)+\nNext:\n\* .+/
+    assert_not_nil(Meuh::Formatting.text(@tracks, false) =~ r)
+    assert_not_nil(Meuh::Formatting.text(@tracks, true) =~ r)
+  end
+
+  def test_general_format_color
+    r = /^.+\n\nPreviously:\n(?:\* \[.*?\] .*\n)+\nNext:\n\* .+/
+    assert_not_nil(Meuh::Formatting.text(@tracks, true) =~ r)
+  end
+
+  def test_color_reset
+    assert_equal(nil, Meuh::Formatting.text(@tracks, false).index("\e[0m"))
+    assert_not_nil(Meuh::Formatting.text(@tracks, true).index "\e[0m")
+  end
+
+end
