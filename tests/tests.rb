60560663115fcdb8b993aac4a1967365efb7cb0e
Baptiste Fontaine <batifon@yahoo.fr>
2014-01-28 11:13:36 +0100

    helpers tests

--- tests/tests.rb
+++ tests/tests.rb
@@ -14,6 +14,13 @@ for t in Dir.glob( File.join( test_dir,  '*_tests.rb' ) )
   require t
 end
 
+class FakeHTMLElement
+  attr_reader :text
+  def initialize(text)
+    @text = text
+  end
+end
+
 class MeuhTests < Test::Unit::TestCase
 
   # == Meuh#version == #
@@ -22,10 +29,51 @@ class MeuhTests < Test::Unit::TestCase
     assert(Meuh.version =~ /^\d+\.\d+\.\d+/)
   end
 
+  # == Meuh#url == #
+  def test_meuh_url
+    assert(Meuh.url =~ /^https?:\/\//)
+  end
+
   # == Meuh#user_agent == #
 
   def test_meuh_ua
     assert(Meuh.user_agent =~ /^Meuh\/\d+\.\d+\.\d+/)
+  end
+
+  # == Meuh#text == #
+
+  def test_meuh_text_empty
+    e = FakeHTMLElement.new ''
+    assert_equal('', Meuh.text(e))
+  end
+
+  def test_meuh_text_spaces
+    e = FakeHTMLElement.new '   '
+    assert_equal('', Meuh.text(e))
+  end
+
+  def test_meuh_text_strip
+    txt = ' foobar  '
+    e = FakeHTMLElement.new txt
+    assert_equal(txt.strip, Meuh.text(e))
+  end
+
+  def test_meuh_text_newlines1
+    txt = "foo\rbar"
+    e = FakeHTMLElement.new txt
+    assert_equal(txt.sub(/\r/, "\n"), Meuh.text(e))
+  end
+
+  def test_meuh_text_newlines2
+    txt = "foo\nbar"
+    e = FakeHTMLElement.new txt
+    assert_equal(txt, Meuh.text(e))
+  end
+
+  def test_meuh_text
+    txt = 'foobar'
+    e = FakeHTMLElement.new txt
+    assert_equal(txt, Meuh.text(e))
   end
 
 end
